243608269c95d37cdfcd2e950e8503a7c2b570c8
RomeoC13 <romeo.chatel@gmail.com>
2020-04-25 20:24:52 +0200

    adding png to Demineur

--- src/Cube.js
+++ src/Cube.js
@@ -3,33 +3,35 @@ import React from "react";
 class Cube extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={i:this.props.i,j:this.props.j}
+        this.state = {i: this.props.i, j: this.props.j}
 
     }
 
 
-    render(){
+    render() {
         let content;
         let buttonClass;
-        if(this.props.show){
-            buttonClass="DemineurButton showed"
-            if(this.props.value===0){
-                content=" "
+        if (this.props.show) {
+            buttonClass = "DemineurButton showed"
+            if (this.props.value === 0) {
+                content = " "
+            } else if (this.props.value === "*") {
+                content = <img src={require('./Utils/Bomb.png')}/>
+            } else {
+                content = this.props.value;
+
             }
-            else {
-                content=this.props.value;
+        } else {
+            buttonClass = "DemineurButton notShowed"
+            if (this.props.flag) {
+                content = <img src={require('./Utils/Flag.png')}/>
+            } else {
+                content = " ";
             }
         }
-        else{
-            buttonClass="DemineurButton notShowed"
-            if(this.props.flag){
-                content ="F";
-            }
-            else{
-                content =" ";
-            }
-        }
-        return <button class={buttonClass} onClick={()=>this.props.click(this.state.i,this.state.j,true)} onContextMenu={()=>this.props.leftClick(this.state.i,this.state.j)}>{content}</button>
+        return <button class={buttonClass} onClick={() => this.props.click(this.state.i, this.state.j, true)}
+                       onContextMenu={() => this.props.leftClick(this.state.i, this.state.j)}>{content}</button>
     }
 }
+
 export {Cube}
